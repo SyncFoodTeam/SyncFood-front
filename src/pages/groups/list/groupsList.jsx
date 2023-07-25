479d557d3ec2b8bc1ee3119aaa88b0ee1cdafc57
Kenji Lao <lao.kenjii@gmail.com>
2023-07-25 16:16:54 +0200

    feat: ajout liste des groupes

--- src/pages/groups/list/groupsList.jsx
+++ src/pages/groups/list/groupsList.jsx
@@ -1,7 +1,8 @@
+import { GetGroupService } from '../../../service/groupe.service';
 import Header from '../../../component/header/header';
 import Menu from '../../../component/menu/menu';
 import './groupsList.css';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 
 
@@ -14,15 +15,41 @@ function GroupsList() {
         navigate('/createGroups');
     }
 
+    const [groups, setGroups] = useState([]);
+
+
+    useEffect(() => {
+        getGroup();
+    }, []);
+
+    async function getGroup() {
+        console.log("getGroup()");
+        let groups = await GetGroupService();
+        setGroups(groups);
+    }
+
+
+
+
     return (
         <div className="App">
 
             <Header barCodeScannerIsTrue={false} />
 
-
             <h1>Groups Page List</h1>
 
             <button onClick={createGroup}>Créer un Groupe</button>
+
+            <div>
+                <ul>
+                    {groups.map((objet) => (
+                    <li key={objet.id}>
+                        <h3>{objet.name}</h3>
+                        <p>{objet.description}</p>
+                    </li>
+                    ))}
+                </ul>
+            </div>
 
 
             <Menu />
